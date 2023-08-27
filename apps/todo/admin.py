1751fe85fa57943c33a8082cd30f85dcdb6d190d
Simon Karapetyan <simon.karapetyan@esterox.am>
2023-08-27 20:44:27 +0400

    Added todos and points tables in db

--- apps/todo/admin.py
+++ apps/todo/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from apps.todo.models import *
 
-# Register your models here.
+
+admin.site.register(Todo)
+admin.site.register(Point)
--- apps/todo/models.py
+++ apps/todo/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Point(models.Model):
+    value = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.value)
+    
+
+class Todo(models.Model):
+    name = models.CharField(max_length=255)
+    done = models.BooleanField(default=False)
+    point_id = models.ForeignKey(Point, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
